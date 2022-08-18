04653cd1f03c52bfb3b9a9326816b22e48acfb94
HsinShan <shirleyhsieh825@gmail.com>
2022-08-18 21:28:25 +0800

    Change header logo

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Navbar from "./navbar";
+import Logo from "../../assets/images/logo.png";
 import "../../assets/style/header/index.scss";
 
 const Header = () => {
   return (
     <div className="header">
       <Link to="/" className="logo">
-        Shirley's Blog
+        <img src={Logo} alt="logo" className="logo-image" />
+        <span className="logo-name">Shirley's Blog</span>
       </Link>
       <Navbar />
     </div>
