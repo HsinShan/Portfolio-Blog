c021b56cc32d529a8db82ab7cdeaaa414554dfdd
HsinShan <shirleyhsieh825@gmail.com>
2022-08-18 16:06:48 +0800

    Initial project component

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Project from "../components/portfolio/project";
 import "../assets/style/portfolio/index.scss";
 
 const Portfolio = () => {
   return (
     <div className="portfolio">
-      <h1></h1>
+      <h1 className="page-title">資料科學相關專案</h1>
+      <Project />
     </div>
   );
 };
