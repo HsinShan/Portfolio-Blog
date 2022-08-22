8ae5c420f2752d40798768f1bcff0e52c9eed08f
HsinShan <shirleyhsieh825@gmail.com>
2022-08-22 16:57:59 +0800

    Adjust project2 & project3 overlay to rwd

--- src/components/portfolio/project2.js
+++ src/components/portfolio/project2.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { Button } from "antd";
+import useDeviceDetect from "../../hooks/useDeviceDetect";
 import "../../assets/style/portfolio/project2.scss";
 import "../../assets/style/portfolio/overlay.scss";
 
 const Project2 = ({ project }) => {
   const { title, subtitle, image, skills, demoURL, codeURL } = project;
+  const device = useDeviceDetect();
+  const overlayClass = device === "laptop" ? "overlay" : "normal";
+
   return (
     <div className="project project2">
       <img className="image" alt={title} src={image} />
-      <div className="overlay">
+      <div className={overlayClass}>
         <div className="subtitle">【{subtitle}】</div>
         <div className="title">{title}</div>
         <div className="skills">
--- src/components/portfolio/project3.js
+++ src/components/portfolio/project3.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Button } from "antd";
+import useDeviceDetect from "../../hooks/useDeviceDetect";
 import "../../assets/style/portfolio/project3.scss";
 import "../../assets/style/portfolio/overlay.scss";
 
 const Project3 = ({ project }) => {
   const { title, image, demoURL, codeURL } = project;
+  const device = useDeviceDetect();
+  const overlayClass = device === "laptop" ? "overlay" : "normal";
   return (
     <div className="project project3">
       <img className="image" alt={title} src={image} />
-      <div className="overlay">
+      <div className={overlayClass}>
         <div className="title">{title}</div>
         <div className="link">
           <Button href={demoURL} target="_blank">
