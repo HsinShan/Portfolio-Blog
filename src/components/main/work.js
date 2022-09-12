2f295685e5af3e60e727c14af86aad1c5dd20ea6
HsinShan <shirleyhsieh825@gmail.com>
2022-09-12 16:31:10 +0800

    Update work experience

--- src/components/main/work.js
+++ src/components/main/work.js
@@ -8,19 +8,22 @@ const Work = () => {
       <Timeline mode="left">
         <Timeline.Item label="2019.11-2020.09" color="blue">
           <h3>前端開發工程師 | 台灣樂天市場</h3>
+          <p>主要使用 React，其他技術 ROR、Graphql</p>
           <p>主要負責專案開發:</p>
           <p>1. 全新商品評價頁面開發</p>
           <p>2. 全新店家促銷活動頁面開發</p>
         </Timeline.Item>
         <Timeline.Item label="2019.03-2019.06" color="blue">
           <h3>軟體開發實習生 | 研華</h3>
-          <p> 物聯網軟體前端功能開發，及協助設備導入 Agent</p>
+          <p>主要使用 Vue 搭配 Vuestic 及 echarts</p>
+          <p>物聯網軟體前端功能開發，及協助設備導入 Agent</p>
           <p>
             <a href="http://bit.ly/31lOjID">實習證書</a>
           </p>
         </Timeline.Item>
         <Timeline.Item label="2018.09-2019.01" color="blue">
           <h3>系統開發實習生 | 若水國際</h3>
+          <p>主要使用 Vue 搭配 ElementUI 及 Axios </p>
           <p>新AI標註平台前端開發及維護現有AI標註平台</p>
         </Timeline.Item>
       </Timeline>
